cb9b279d3f593aec9a9d61e84b9b81fac08fcc8b
chacoff <jaime.arctico@gmail.com>
2023-02-04 22:41:35 +0100

    change in the learning rate for testing

--- demo.py
+++ demo.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser(description='PyTorch Unsupervised Segmentation'
 parser.add_argument('--nChannel', metavar='N', default=100, type=int,  help='number of channels')
 parser.add_argument('--maxIter', metavar='T', default=1000, type=int, help='number of maximum iterations')
 parser.add_argument('--minLabels', metavar='minL', default=3, type=int,  help='minimum number of labels')
-parser.add_argument('--lr', metavar='LR', default=0.1, type=float, help='learning rate')
+parser.add_argument('--lr', metavar='LR', default=0.5, type=float, help='learning rate')
 parser.add_argument('--nConv', metavar='M', default=2, type=int, help='number of convolutional layers')
 parser.add_argument('--num_superpixels', metavar='K', default=10000, type=int,  help='number of superpixels')
 parser.add_argument('--compactness', metavar='C', default=100, type=float, help='compactness of superpixels')
