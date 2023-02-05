0b9c1d7f949dd92bd1a24c308e763efea8fd9b1d
chacoff <jaime.arctico@gmail.com>
2023-02-05 02:19:38 +0100

    litle bit more testing

--- demo.py
+++ demo.py
@@ -17,14 +17,14 @@ use_cuda = torch.cuda.is_available()
 
 parser = argparse.ArgumentParser(description='PyTorch Unsupervised Segmentation')
 parser.add_argument('--nChannel', metavar='N', default=100, type=int,  help='number of channels')
-parser.add_argument('--maxIter', metavar='T', default=1000, type=int, help='number of maximum iterations')
-parser.add_argument('--minLabels', metavar='minL', default=3, type=int,  help='minimum number of labels')
-parser.add_argument('--lr', metavar='LR', default=0.5, type=float, help='learning rate')
+parser.add_argument('--maxIter', metavar='T', default=150, type=int, help='number of maximum iterations')
+parser.add_argument('--minLabels', metavar='minL', default=4, type=int,  help='minimum number of labels')
+parser.add_argument('--lr', metavar='LR', default=0.05, type=float, help='learning rate')
 parser.add_argument('--nConv', metavar='M', default=2, type=int, help='number of convolutional layers')
 parser.add_argument('--num_superpixels', metavar='K', default=10000, type=int,  help='number of superpixels')
 parser.add_argument('--compactness', metavar='C', default=100, type=float, help='compactness of superpixels')
 parser.add_argument('--visualize', metavar='1 or 0', default=1, type=int, help='visualization flag')
-parser.add_argument('--input', metavar='FILENAME', help='input image file name', required=True)
+parser.add_argument('--input', metavar='FILENAME', default='images/beam.png', help='input image file name')
 args = parser.parse_args()
 
 
@@ -56,9 +56,14 @@ class MyNet(nn.Module):
 
 
 # load image
-im = cv2.imread(args.input)
+try:
+    im = cv2.imread(args.input)
+except RuntimeError as e:
+    print(args.input, e)
+
 data = torch.from_numpy( np.array([im.transpose( (2, 0, 1) ).astype('float32')/255.]) )
 if use_cuda:
+    print(use_cuda)
     data = data.cuda()
 data = Variable(data)
 
